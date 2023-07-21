15d6aa6f97699116cc45dc0714e2f2d26d60fe14
mhirdes <Marc_Hirdes@gmx.de>
2023-07-21 10:05:55 +0200

    [TASK] Update ext_emconf constraints for TYPO3 12

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
   'constraints' => [
     'depends' => [
       'php' => '7.2.0-8.999.999',
-      'typo3' => '9.5.31-11.5.99',
+      'typo3' => '9.5.31-12.4.99',
     ],
     'conflicts' => [
     ],
